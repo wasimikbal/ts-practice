ee30259b96a08ba4464756b0b4de589d1057e1e5
wasimikbal <44136359+wasimikbal@users.noreply.github.com>
2024-05-30 03:00:38 +0500

    installed tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
   );
 }
