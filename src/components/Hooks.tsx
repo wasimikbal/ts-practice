833c6c88e26cded6a1ad1ed0963ddd390f7ee6e9
wasimikbal <44136359+wasimikbal@users.noreply.github.com>
2024-05-30 17:08:29 +0500

    typing useRef

--- src/components/Hooks.tsx
+++ src/components/Hooks.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 
 type User = {
     name: string,
@@ -9,9 +9,12 @@ type User = {
 const Hooks = () => {
     const [user, setUser] = useState<User | null>(null) // Initially it will be null until fetched and set
 
+    const ref = useRef<HTMLButtonElement | null>(null);
+
 
   return (
     <>
+    <button ref={ref}></button>
     </>
   )
 }
