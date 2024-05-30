c5b7c557e4d07bb6f42da142ae3d602385e07354
wasimikbal <44136359+wasimikbal@users.noreply.github.com>
2024-05-30 13:00:21 +0500

    typing functions

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
 const Button = () => {
-    let url: string = "www.google.com";
+    
+  const DoSomething = (name: string, age: number): string => {
+    return "string to be returned";
+  }
 
   return (
     <button className='bg-blue-500 text-white rounded px-4 py-2'>Click Me</button>
