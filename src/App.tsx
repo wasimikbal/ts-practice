abe9022443df9504b159fbc9432fca408f98269f
wasimikbal <44136359+wasimikbal@users.noreply.github.com>
2024-05-30 12:57:05 +0500

    typing variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
+import Button from './components/Button';
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      <Button/>
     </div>
   );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Button = () => {
+    let url: string = "www.google.com";
+
+  return (
+    <button className='bg-blue-500 text-white rounded px-4 py-2'>Click Me</button>
+  )
+}
+
+export default Button
