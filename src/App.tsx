99c63cd6b17e587241a7dd70c7393146db47a3ba
wasimikbal <44136359+wasimikbal@users.noreply.github.com>
2024-05-30 13:57:51 +0500

    React.CSSProperties

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Button from './components/Button';
 function App() {
   return (
     <div className="App">
-      <Button backgroundColor='bg-blue-500' fontSize={30} padding={[10,10,10,10]}/>
+      <Button backgroundColor='bg-blue-500' textColor='bg-black-500' fontSize={30} padding={[10,10,10,10]}/>
     </div>
   );
 }
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,24 +1,30 @@
 import React from 'react'
 
 //Union type
-type color = 'bg-blue-500' | 'bg-green-500' | 'bg-red-500'
+type color = 'bg-blue-500' | 'bg-green-500' | 'bg-black-500'
 
 //Button props object
 type ButtonProps = {
   backgroundColor: color,
+  textColor: color,
   fontSize: number,
   pillShape?: boolean
   padding: [number, number, number, number]
 }
 
-const Button = ({backgroundColor, fontSize, pillShape}: ButtonProps) => {
+const Button = ({backgroundColor, textColor, fontSize, pillShape, padding}: ButtonProps) => {
     
   const DoSomething = (name: string, age: number): string => {
     return "string to be returned";
   }
 
   return (
-    <button className={`${backgroundColor} text-white rounded px-4 py-2`}>Click Me</button>
+    <button style={{
+      backgroundColor : backgroundColor,
+      color: textColor,
+      fontSize: fontSize,
+      padding: `${padding[0]}px ${padding[1]}px ${padding[2]}px ${padding[3]}px`
+    }}>Click Me</button>
   )
 }
 
