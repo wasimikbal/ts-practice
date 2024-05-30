94cc7ce36e94aff47508e7a0b3122dad203639f2
wasimikbal <44136359+wasimikbal@users.noreply.github.com>
2024-05-30 19:35:41 +0500

    Generics

--- src/components/Hooks.tsx
+++ src/components/Hooks.tsx
@@ -1,27 +1,44 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState } from "react";
 
 type User = {
-    name: string,
-    sessionId: string
-}
+  name: string;
+  sessionId: string;
+};
 
-type Guest = Omit<User, 'name'> // leave the user with just sessionId
-
+type Guest = Omit<User, "name">; // leave the user with just sessionId
 
 const Hooks = () => {
+  const states = ["Awake", "Sleep", "Working"] as const; // make the array readonly
 
-    const states = ['Awake', 'Sleep', 'Working'] as const // make the array readonly 
+  const [user, setUser] = useState<User | null>(null); // Initially it will be null until fetched and set
 
-    const [user, setUser] = useState<User | null>(null) // Initially it will be null until fetched and set
+  const ref = useRef<HTMLButtonElement | null>(null);
 
-    const ref = useRef<HTMLButtonElement | null>(null);
+  //---------------------------------------------------------------------------------------------------------------------/
+  //    Generics
+  //---------------------------------------------------------------------------------------------------------------------/
 
+
+  
+  // To store an element in an array
+  const ConvertToArray = <K,>(value: K): K[] => 
+    {
+        return [value];
+    }
+    
+    ConvertToArray(10);
+
+    // To get first element from an array
+    const GetFirstElementFromArray = <T,> (value: T[]) : T => {
+        return value[0];
+    }
+  
 
   return (
     <>
-    <button ref={ref}></button>
+      <button ref={ref}></button>
     </>
-  )
-}
+  );
+};
 
-export default Hooks
+export default Hooks;
